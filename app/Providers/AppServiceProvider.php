e76744ecfbe82d33f6b8978f699c248886281f17
mendiltanggo <mendiltanggo@gmail.com>
2026-07-23 10:58:03 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Tambahan untuk memanggil fitur URL
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // <-- Logika tambahan untuk mengatasi Mixed Content (Memaksa HTTPS)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
